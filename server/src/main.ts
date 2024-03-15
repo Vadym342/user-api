f8a61f6f219f4e79534f3eea5fafee823294ed19
Vadym <vadimca1.boyarchuc@gmail.com>
2024-03-15 19:22:47 +0200

    feat(UAP-3): init swagger doc

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,12 +5,14 @@ import { config } from 'dotenv';
 import { Logger } from 'nestjs-pino';
 
 import { AppModule } from './modules/app/app.module';
+import { useSwagger } from './utils/swagger';
 
 config();
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { bufferLogs: true });
 
+  useSwagger(app);
   app.useLogger(app.get(Logger));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
